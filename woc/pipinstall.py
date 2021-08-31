ad269dace1b5b9b7ac9246fdb37288f233e4fd8a
iszj <szj2ys@163.com>
2021-08-31 15:57:08 +0800

    2021-08-31 15:57:07

--- woc/pipinstall.py
+++ woc/pipinstall.py
@@ -3,8 +3,16 @@ import fire
 import os
 
 
-def install_package(pkg):
-    os.system(f'pip3 install {pkg} -i https://mirrors.aliyun.com/pypi/simple')
+def install_package(*args):
+
+    if 'o' in args:
+        for pkg in [pkg for pkg in args if pkg not in ['o']]:
+            os.system(
+                f'pip3 install {pkg} -i https://mirrors.aliyun.com/pypi/simple'
+            )
+    else:
+        for pkg in [pkg for pkg in args if pkg not in ['o']]:
+            os.system(f'pip3 install {pkg}')
 
 
 def main():
