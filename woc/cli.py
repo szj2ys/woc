4d51501980f6d9aba730005a642142bd08aef901
szj2ys <15574364821@163.com>
2021-08-31 17:31:14 +0800

    2021-08-31 17:31:14

--- woc/cli.py
+++ woc/cli.py
@@ -105,6 +105,23 @@ def hexod():
 @cli.command(help='print alias')
 def alias():
     ALIASES = '''
+alias ll='ls -lh'
+alias l='ls -lha'
+alias ..='cd ..'
+alias axel='axel -n 100'
+alias free="free -h"
+alias vi="vim"
+alias ~="cd ~"
+alias sh="bash"
+
+# pathes
+alias cddow="cd ~/Downloads/;pwd"
+alias cdh="cd ~;pwd"
+alias cddoc="cd ~/Documents/;pwd"
+alias cdmusic="cd ~/Music;pwd"
+alias cdgitee="cd ~/Music/GiteProjects/"
+alias cdgithub="cd ~/Music/GithubProjects/"
+
 #alias jp="jupyter notebook --no-browser --allow-root >&1 &"
 alias jp="jupyter notebook --no-browser"
 
@@ -113,14 +130,7 @@ alias jl="jupyter lab"
 
 # start mongodb server
 alias imongo="sudo /usr/local/mongodb/bin/mongod --dbpath=/usr/local/mongodb/data/db/"
-
-alias cddow="cd ~/Downloads/;pwd"
-alias cdh="cd ~;pwd"
-alias cddoc="cd ~/Documents/;pwd"
-alias cdmusic="cd ~/Music;pwd"
-alias cdgitee="cd ~/Music/GiteProjects/"
-alias cdgithub="cd ~/Music/GithubProjects/"
-    
+   
 '''
     print(ALIASES)
 
